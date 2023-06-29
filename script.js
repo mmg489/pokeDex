57ac0aa04252c56b56f74d0181cb7bb8d9574aab
Magi Munoz <m_mg489@outlook.com>
2023-06-28 23:52:52 -0400

    trying out a different request process in JS

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const pokeEl =document.getElementById('pokeData')
-const pokeBtn =document.getElementById('searchBtn')
+// const pokeEl =document.getElementById('pokeData')
+// const pokeBtn =document.getElementById('searchBtn')
 
-async function pokeSearch() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
-    const data = await response.json()
+// async function pokeSearch() {
+//   var input = document.getElementById("pokemonInput").value;
+//   var url = "https://pokeapi.co/api/v2/pokemon/" + input;
+
+
+//     const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+//     const data = await response.json()
     
-    // pokeEl.innerHTML = data.species.name
+//     // pokeEl.innerHTML = data.species.name
 
-    console.log(data);   
+//     console.log(data);   
 
-  }
+  // }
 
   pokeSearch();
 
@@ -20,3 +24,23 @@ async function pokeSearch() {
 //     // jokeEl.innerHTML = data.joke
 // })
 
+function searchPokemon() {
+  var input = document.getElementById("pokemonInput").value;
+  var url = "https://pokeapi.co/api/v2/pokemon/" + input;
+
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var pokemonData = "<h2>" + data.name + "</h2>";
+      pokemonData += "<img src='" + data.sprites.front_default + "' alt='Pokemon Image'>";
+      pokemonData += "<p>Height: " + data.height + "</p>";
+      pokemonData += "<p>Weight: " + data.weight + "</p>";
+
+      document.getElementById("pokemonData").innerHTML = pokemonData;
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
